872f42e2b38fcc4fbedc1e282c8a1a57008f6eeb
Paul Biermann <paulmb@iastate.edu>
2018-01-26 17:18:53 -0600

    Added HelloController.java

---
+++ paulExperiments/myProject/src/main/java/myProject/HelloController.java
@@ -0,0 +1,14 @@
+package myProject;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+	
+	@RequestMapping("/hello")
+	public String sayHi() {
+		return "Hi!";
+	}
+	
+}
