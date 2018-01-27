a54d4e784376ef8f244f8b41dfa3e56110af0036
Mike Onyszczak <mikeonys@iastate.edu>
2018-01-27 14:02:07 -0600

    Added Numeric Inputs, Input type checking, and toasts to echo input for the second input line

--- MikeExperiment/app/src/main/java/mikeonys/mikeexperiment/MainActivity.java
+++ MikeExperiment/app/src/main/java/mikeonys/mikeexperiment/MainActivity.java
@@ -1,10 +1,12 @@
 package mikeonys.mikeexperiment;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "mikeonys.mikeexperiment.MESSAGE"; //Needed to send intent messages
@@ -21,5 +23,26 @@ public class MainActivity extends AppCompatActivity {
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
+    }
+
+    public void sendScore(View view){
+        Context context = getApplicationContext();
+        CharSequence text;
+        int duration = Toast.LENGTH_SHORT;
+
+        EditText editText = (EditText) findViewById(R.id.editText2);
+        try {
+            int score = Integer.parseInt(editText.getText().toString());
+            text = score + ": Entered";
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+
+        }catch (NumberFormatException e){
+            text = "Invalid Input: Enter only integers";
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }
     }
 }
