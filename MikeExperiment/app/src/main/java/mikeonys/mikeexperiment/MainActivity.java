ac576e8ac1b94a87b1f8810607fb4a842698e36b
Mike Onyszczak <mikeonys@iastate.edu>
2018-01-27 14:33:47 -0600

    Added storage using sharedPrefs, 2nd Line input will record the max and print below with reset button

--- MikeExperiment/app/src/main/java/mikeonys/mikeexperiment/MainActivity.java
+++ MikeExperiment/app/src/main/java/mikeonys/mikeexperiment/MainActivity.java
@@ -2,19 +2,35 @@ package mikeonys.mikeexperiment;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "mikeonys.mikeexperiment.MESSAGE"; //Needed to send intent messages
+    SharedPreferences sharedPreferences;
+    public static final String mypreference = "mypref";
+    public static final String scoreKey = "scoreKey";
+    int hiScore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sharedPreferences = getSharedPreferences(mypreference, Context.MODE_PRIVATE);
+
+        if(sharedPreferences.contains(scoreKey)) {
+            hiScore = sharedPreferences.getInt(scoreKey, 0);
+        }
+        else
+            hiScore = 0;
+
+        TextView textView = findViewById(R.id.textView2);
+        textView.setText("Hi-Score: " + hiScore);
     }
 
     public void sendMessage(View view) {
@@ -38,11 +54,34 @@ public class MainActivity extends AppCompatActivity {
             Toast toast = Toast.makeText(context, text, duration);
             toast.show();
 
+            submitScore(score);
+
         }catch (NumberFormatException e){
             text = "Invalid Input: Enter only integers";
 
             Toast toast = Toast.makeText(context, text, duration);
             toast.show();
         }
+        TextView textView = findViewById(R.id.textView2);
+        textView.setText("Hi-Score: " + hiScore);
+    }
+
+    public void submitScore(int score){
+        if(score > hiScore){
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putInt(scoreKey, score);
+            editor.apply();
+            hiScore = score;
+        }
+
+    }
+
+    public void resetScore(View view){
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.remove(scoreKey);
+        editor.apply();
+        hiScore = 0;
+        TextView textView = findViewById(R.id.textView2);
+        textView.setText("Hi-Score: " + hiScore);
     }
 }
