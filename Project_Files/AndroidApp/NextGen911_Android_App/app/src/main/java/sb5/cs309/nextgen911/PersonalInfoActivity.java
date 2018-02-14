4d1779fda57fd8314ef8c07164d63d0a1b2b3cf8
Mike Onyszczak <mikeonys@iastate.edu>
2018-02-14 13:17:55 -0600

    Added Date Parsing

--- Project_Files/AndroidApp/NextGen911_Android_App/app/src/main/java/sb5/cs309/nextgen911/PersonalInfoActivity.java
+++ Project_Files/AndroidApp/NextGen911_Android_App/app/src/main/java/sb5/cs309/nextgen911/PersonalInfoActivity.java
@@ -8,6 +8,7 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,7 +16,10 @@ import org.w3c.dom.Text;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
+
 
 public class PersonalInfoActivity extends AppCompatActivity {
 
@@ -68,23 +72,36 @@ public class PersonalInfoActivity extends AppCompatActivity {
     public void submit_info(View view){
         check_DOB();
 
-        Context context = getApplicationContext();
-        CharSequence text = "Changes Submitted";
-        int duration = Toast.LENGTH_SHORT;
-
-        Toast toast = Toast.makeText(context, text, duration);
-        toast.show();
+        
     }
 
     public void check_DOB(){
-        String dtStart = ;
-        SimpleDateFormat format = new SimpleDateFormat("dd/MM-yyyy");
+        EditText dob_box = findViewById(R.id.dob_box);
+        String dtStart = dob_box.getText().toString();
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy", Locale.ENGLISH);
+
+
+
+
         try {
-            Date date = format.parse(dtStart);
-            System.out.println(date);
+            cal.setTime(sdf.parse(dtStart));
+            Context context = getApplicationContext();
+            CharSequence text = cal.get(cal.MONTH)+1 + "/" + cal.get(cal.DAY_OF_MONTH) + "/" + cal.get(cal.YEAR);
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+
         } catch (ParseException e) {
-            e.printStackTrace();
+            Context context = getApplicationContext();
+            CharSequence text = "Invalid Date Format";
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
         }
+
     }
 
 }
