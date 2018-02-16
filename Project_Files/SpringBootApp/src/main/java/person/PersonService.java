e62b140f244429e5c4adbd5a4111bdff94e66e1c
Paul Biermann <paulmb@iastate.edu>
2018-02-16 09:17:39 -0600

    added getAllPersons() to PersonService.java

--- Project_Files/SpringBootApp/src/main/java/person/PersonService.java
+++ Project_Files/SpringBootApp/src/main/java/person/PersonService.java
@@ -1,13 +1,25 @@
 package person;
 
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class PersonService {
 	
+	public List<Person> getAllPersons() {
+		return new ArrayList<>(Arrays.asList(
+				new Person(123),
+				new Person(456),
+				new Person(789)
+				));
+	}
 	
 	public Person getPerson(int id) {
-		//TODO retrieve a person from database
 		return new Person(id);
 	}
+	
+	
 }
