3266538b3e39b0749f1a5c3faf5323c59d0d1d5e
Paul Biermann <paulmb@iastate.edu>
2018-02-20 16:38:49 -0600

    added location field to operator

--- Project_Files/SpringBootApp/src/main/java/person/Operators.java
+++ Project_Files/SpringBootApp/src/main/java/person/Operators.java
@@ -6,27 +6,29 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "operators")
-public class Operators 
+public class Operators
 {
 	@Id
 	private String id;
-	private String userName; 
-	private String password; 
-	private String firstName; 
-	private String lastName; 
-	private String status; 
-	
-	public Operators(String id, String userName, String password, String firstName, String lastName, String status)
+	private String userName;
+	private String password;
+	private String firstName;
+	private String lastName;
+	private String status;
+	private String location;
+
+	public Operators(String id, String userName, String password, String firstName, String lastName, String status, String location)
 	{
 		super();
-		this.id = id; 
-		this.userName = userName; 
-		this.password = password; 
-		this.firstName = firstName; 
+		this.id = id;
+		this.userName = userName;
+		this.password = password;
+		this.firstName = firstName;
 		this.lastName = lastName;
-		this.status = status; 
+		this.status = status;
+		this.location = location;
 	}
-	
+
 	public Operators(String id)
 	{
 		super();
@@ -35,68 +37,77 @@ public class Operators
 		firstName = "";
 		lastName = "";
 		this.id = id;
+		location = "";
 	}
-	
+
 	public Operators() {
 		super();
 	}
-	
+
+	public void setLocation(String location)
+	{
+		this.location = location;
+	}
+	public String getLocation()
+	{
+		return location;
+	}
 	public void setID(String id)
 	{
 		this.id = id;
 	}
-	
+
 	public String getID()
 	{
 		return id;
 	}
-	
+
 	public void setUserName(String userName)
 	{
 		this.userName = userName;
 	}
-	
+
 	public String getUserName()
 	{
 		return userName;
 	}
-	
+
 	public void setPassword(String password)
 	{
 		this.password = password;
 	}
-	
+
 	public String getPassword()
 	{
 		return password;
 	}
-	
+
 	public void setFirstName(String firstName)
 	{
 		this.firstName = firstName;
 	}
-	
+
 	public String getFirstName()
 	{
 		return firstName;
 	}
-	
+
 	public void setLastName(String lastName)
 	{
 		this.lastName = lastName;
 	}
-	
+
 	public String getLastName()
 	{
 		return lastName;
 	}
-	
+
 	public void setStatus(String status)
 	{
 		this.status = status;
 	}
-	
-	public String getStatus() 
+
+	public String getStatus()
 	{
 		return status;
 	}
