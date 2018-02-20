adbf3145f00acdae6c4f3d455a6a05bea22dfae6
Shubham Sharma <shubham@iastate.edu>
2018-02-20 17:33:09 -0600

    Complted the Windows Project for DEMO2

--- Project_Files/WindowsApp/Operator911_App/src/operator/Models/OperatorModel.java
+++ Project_Files/WindowsApp/Operator911_App/src/operator/Models/OperatorModel.java
@@ -1,14 +1,58 @@
 package operator.Models;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class OperatorModel {
 
-    
-    private int id;
+    private String id;
     private String userName;
     private String password;
     private String firstName;
     private String lastName;
-    private int status;
+    private String location;
+    private String status;
+
+    public OperatorModel(JSONObject jsonObject) throws JSONException {
+        id = jsonObject.getString("id");
+        userName = jsonObject.getString("userName");
+        password = jsonObject.getString("password");
+        firstName = jsonObject.getString("firstName");
+        lastName = jsonObject.getString("lastName");
+        location = jsonObject.getString("location");
+        status = jsonObject.getString("status");
+
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
 
 
 
