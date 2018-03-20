f917d06166fe5b3f703e477147eb59fd6dd37267
rishabn <rishabn@iastate.edu>
2018-03-20 22:14:40 +0000

    Update Login.java

--- Project_Files/SpringBootApp/src/main/java/person/Login.java
+++ Project_Files/SpringBootApp/src/main/java/person/Login.java
@@ -23,6 +23,20 @@ public class Login {
 		this.location = location;
 		this.status = status;
 	}
+	
+	public Login(String id)
+	{
+		super();
+		firstName = "";
+		lastName = "";
+		accesibility = "";
+		userName = "";
+		password = "";
+		this.id = id;
+		location = "";
+		status = "";
+	}
+
 
 	public void setID(String id)
 	{
