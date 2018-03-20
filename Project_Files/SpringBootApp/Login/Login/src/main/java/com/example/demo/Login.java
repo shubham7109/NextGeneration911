7d76fef29bbfaee7902224a503eff9f6891e21c6
rishabnarendra <risnarendra@gmail.com>
2018-03-20 13:22:07 -0500

    Created new instance variables

---
+++ Project_Files/SpringBootApp/Login/Login/src/main/java/com/example/demo/Login.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+public class Login {
+
+	private String firstName;
+	private String lastName;
+	private String accesibility; 
+	private String userName;
+	private String password;
+	private String location;
+	private int status;
+}
