baa4d23bf2103d2b471221474b61bd6bf67b559e
rishabnarendra <risnarendra@gmail.com>
2018-03-24 15:11:00 -0500

    Implemented Deploy.java

--- Project_Files/SpringBootApp/Deploy/src/main/java/com/example/demo/Deploy.java
+++ Project_Files/SpringBootApp/Deploy/src/main/java/com/example/demo/Deploy.java
@@ -5,8 +5,55 @@ public class Deploy {
 	private String id; 
 	// Ambulance, fire brigade, first responders, state troopers, county officers, swat team
 	private String type;
-	private String latitiude;
+	private String latitude;
 	private String longitude; 
 	
+	public Deploy(String id, String type, String latitude, String longitude)
+	{
+		super();
+		this.id = id;
+		this.type = type;
+		this.latitude = latitude;
+		this.longitude = longitude; 
+	}
+
+	public void setID(String id)
+	{
+		this.id = id;
+	}
 	
+	public String getID()
+	{
+		return id; 
+	}
+	
+	public void setType(String type)
+	{
+		this.type = type;
+	}
+
+	public String getType()
+	{
+		return type;
+	}
+
+	public void setLatitude(String latitude)
+	{
+		this.latitude = latitude;
+	}
+
+	public String getLatitude()
+	{
+		return latitude;
+	}
+	
+	public void setLongitude(String longitude)
+	{
+		this.longitude = longitude;
+	}
+
+	public String getLongitude()
+	{
+		return longitude;
+	}
 }
