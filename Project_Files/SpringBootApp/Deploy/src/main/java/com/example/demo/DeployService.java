2f8e38266955fdf21d5f23af48e5f21b72d533da
rishabnarendra <risnarendra@gmail.com>
2018-03-24 15:56:00 -0500

    Updated DeployService.java

--- Project_Files/SpringBootApp/Deploy/src/main/java/com/example/demo/DeployService.java
+++ Project_Files/SpringBootApp/Deploy/src/main/java/com/example/demo/DeployService.java
@@ -1,64 +1,40 @@
 package com.example.demo;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DeployService {
 
-	private List<Deploy> deploy = new ArrayList<>(Arrays.asList(
-			new Deploy("1", "Ambulance", "-38.16157", "-61.21903"),
-			new Deploy("2", "Fire Brigade", "1.70208", "-30.65703"),
-			new Deploy("3", "First Responders", "-57.92470", "-52.92323"),
-			new Deploy("4", "State Troopers", "-3.32945", "-166.53464"),
-			new Deploy("5", "County Officers", "13.21004", "162.12095"),
-			new Deploy("6", "Swat Team", "10.72418", "35.40650")
-			));
+	@Autowired
+	private DeployRepository deployRepository; 
 	
 	public List<Deploy> getAllDeploys() 
 	{
-		return deploy;
+		List<Deploy> deploys = new ArrayList<>();
+		deployRepository.findAll().forEach(deploys::add);
+		return deploys;
 	}
 	
 	public Deploy getDeploy(String id) 
 	{
-		for(int i = 0; i < deploy.size(); i++)
-		{
-			if(deploy.get(i).getID().equals(id))
-				return deploy.get(i); 
-		}
-		return null;
+		return deployRepository.findOne(id);
 	}
 	
 	public void addDeploy(Deploy dep) {
-		deploy.add(dep);
+		deployRepository.save(dep);
 	}
 	
 	public void updateDeploy(String id, Deploy dep) 
 	{
-		for (int i = 0; i < deploy.size(); i++) 
-		{
-			Deploy o = deploy.get(i);
-			if (o.getID() == id) 
-			{
-				deploy.set(i, dep);
-				return;
-			}
-		}
+		deployRepository.save(dep);
 	}
 	
 	public void deleteDeploy(String id) 
 	{
-		for (int i = 0; i < deploy.size(); i++) 
-		{
-			Deploy o = deploy.get(i);
-			if (o.getID() == id) 
-			{
-				deploy.remove(i);
-			}
-		}
+		deployRepository.delete(id);
 	}
 }
