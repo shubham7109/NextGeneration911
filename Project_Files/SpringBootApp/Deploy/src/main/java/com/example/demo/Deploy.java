39ac46fdc868dd975bffa2b567cbb0dce560f01e
rishabnarendra <risnarendra@gmail.com>
2018-03-24 15:06:40 -0500

    New Deploy class

---
+++ Project_Files/SpringBootApp/Deploy/src/main/java/com/example/demo/Deploy.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+public class Deploy {
+
+	private String id; 
+	// Ambulance, fire brigade, first responders, state troopers, county officers, swat team
+	private String type;
+	private String latitiude;
+	private String longitude; 
+	
+	
+}
