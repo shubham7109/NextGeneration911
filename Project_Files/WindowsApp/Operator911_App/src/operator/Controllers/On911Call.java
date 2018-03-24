62de5c5b3613da884e1b5ffcdd4d5f2ff4844182
Shubham Sharma <shubham@iastate.edu>
2018-03-24 01:07:11 -0500

    Designed app to send messages using sockets locally

--- Project_Files/WindowsApp/Operator911_App/src/operator/Controllers/On911Call.java
+++ Project_Files/WindowsApp/Operator911_App/src/operator/Controllers/On911Call.java
@@ -8,9 +8,15 @@ import com.lynden.gmapsfx.service.directions.DirectionsResult;
 import com.lynden.gmapsfx.service.directions.DirectionsService;
 import com.lynden.gmapsfx.service.directions.DirectionsServiceCallback;
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.stage.Stage;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import operator.Client;
+import operator.NetworkConnection;
+import operator.Server;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -22,9 +28,18 @@ public class On911Call implements Initializable, MapComponentInitializedListener
     protected GoogleMapView mapView;
     @FXML
     private URL location;
+    @FXML
+    private TextArea messages;
+    @FXML
+    private TextField input;
+
+
 
     private double LAT = 42.033996;
     private double LONG = -93.641397;
+
+    private boolean isServer = false;
+    private NetworkConnection connection = isServer ? createServer() : createClient();
 
     @Override
     public void mapInitialized() {
@@ -53,9 +68,39 @@ public class On911Call implements Initializable, MapComponentInitializedListener
 
     }
 
+    private Server createServer(){
+        return new Server(5555,data ->{
+            Platform.runLater(()->{
+                messages.appendText(data.toString()+"\n");
+            });
+        });
+    }
+
+    private Client createClient(){
+        return new Client("10.36.40.35", 5555, data ->{
+            messages.appendText(data.toString() + "\n");
+        });
+    }
+
+    @FXML
+    public void onEnter(ActionEvent ae) throws Exception {
+        String message = isServer ? "Server: " : "Client: ";
+        message += input.getText();
+        input.setText("");
+
+        messages.appendText(message + "\n");
+        connection.send(message);
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         mapView.addMapInializedListener(this);
+        try {
+            connection.startConnection();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+
 }
 
