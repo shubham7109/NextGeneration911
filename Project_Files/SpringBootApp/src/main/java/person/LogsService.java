6d56c7ad810085012ac87fa14220dce6ea899f50
Rishab Narendra <rishabn@iastate.edu>
2018-03-25 12:00:14 -0500

    Updated LogsService.java so post request can be made

--- Project_Files/SpringBootApp/src/main/java/person/LogsService.java
+++ Project_Files/SpringBootApp/src/main/java/person/LogsService.java
@@ -1,34 +1,27 @@
-package person;import java.util.Arrays;
+package person;
+
+import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LogsService {
-	
-	private List<Logs> logs = new ArrayList<>(Arrays.asList(
-			new Logs("01/01/1998","10:56","16","George Bush","515-987-6543"),
-			new Logs("02/02/2018","11:01","10","Donald Trump","765-765-7654"),
-			new Logs("03/03/1666","12:54","76","George Washington","987-876-7654"),
-			new Logs("01/01/1998","10:56","16","George Bush","515-987-6543"),
-			new Logs("02/02/2018","11:01","10","Donald Trump","765-765-7654"),
-			new Logs("03/03/1666","12:54","76","George Washington","987-876-7654"),
-			new Logs("01/01/1998","10:56","16","George Bush","515-987-6543"),
-			new Logs("02/02/2018","11:01","10","Donald Trump","765-765-7654"),
-			new Logs("03/03/1666","12:54","76","George Washington","987-876-7654"),
-			new Logs("01/01/1998","10:56","16","George Bush","515-987-6543"),
-			new Logs("02/02/2018","11:01","10","Donald Trump","765-765-7654"),
-			new Logs("03/03/1666","12:54","76","George Washington","987-876-7654")
-			));
+
+	@Autowired
+	private LogsRepository logsRepository;
 	
 	public List<Logs> getAllOperators() {
+
+		List<Login> logs = new ArrayList<>();
+		logsRepository.findAll().forEach(logs::add);
 		return logs;
 	}
-	
-// 	To be implemented for future build of app
-//	public void addOperator(Logs operators) {
-//		logs.add(operators);
-//	}
+
+	public void addOperators(Logs log) {
+		loginRepository.save(log);
+	}
 }
 
