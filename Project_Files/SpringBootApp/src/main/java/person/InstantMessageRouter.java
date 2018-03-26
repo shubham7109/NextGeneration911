bc5de812433671dd13efd4b934494adaf7d7895f
Paul Biermann <paulmb@iastate.edu>
2018-03-26 16:21:53 -0500

    Updated deploy server instructions in SpringBootApp/README

--- Project_Files/SpringBootApp/src/main/java/person/InstantMessageRouter.java
+++ Project_Files/SpringBootApp/src/main/java/person/InstantMessageRouter.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class InstantMessageRouter {
 	
 	@Autowired
@@ -16,13 +18,14 @@ public class InstantMessageRouter {
 		 * return the ip address of that operator */
 		Login oper = getAvailableOperator();
 		
-		/* if the id of the operator is not null,
+		/* if the id of the caller is not null,
 		 * 	then send it to the windows operator*/
 		
 		// this will return null if there is no available operator
 		return oper.getIpAddress();
 	}
 	
+	/* return the first available operator whose status is 0 (who is available) */
 	private Login getAvailableOperator() {
 		List<Login> operators = loginService.getAllLogins();
 		for (int i = 0; i < operators.size(); i++) {
