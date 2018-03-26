3bc7f62c953d0cc512615c83e8cf8906286d29fa
Paul Biermann <paulmb@iastate.edu>
2018-03-26 15:56:21 -0500

    implemented /makecall in InstantMessageRouter.java

--- Project_Files/SpringBootApp/src/main/java/person/InstantMessageRouter.java
+++ Project_Files/SpringBootApp/src/main/java/person/InstantMessageRouter.java
@@ -1,12 +1,33 @@
 package person;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 public class InstantMessageRouter {
-
+	
+	@Autowired
+	private LoginService loginService;
+	
 	@RequestMapping("/makecall")
 	public String makeCall() {
-		return "";
+		/* find the first operator whose status = x
+		 * return the ip address of that operator */
+		Login oper = getAvailableOperator();
+		
+		// this will return null if there is no available operator
+		return oper.getIpAddress();
+	}
+	
+	private Login getAvailableOperator() {
+		List<Login> operators = loginService.getAllLogins();
+		for (int i = 0; i < operators.size(); i++) {
+			if (operators.get(i).getStatus() == 0) {
+				return operators.get(i);
+			}
+		}
+		return new Login();
 	}
 
 }
