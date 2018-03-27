90caea812855fd9154b57e4dc53ffbf48524cd59
Shubham Sharma <shubham@iastate.edu>
2018-03-26 20:00:55 -0500

    Finished text socket proj

--- Project_Files/WindowsApp/StocketTest/src/sample/Main.java
+++ Project_Files/WindowsApp/StocketTest/src/sample/Main.java
@@ -1,6 +1,7 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -9,12 +10,60 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 public class Main extends Application {
 
+    private boolean isServer = false;
+    private NetworkConnection connection = isServer ? createServer() : createClient();
     private TextArea messages = new TextArea();
+
+    public Main() throws UnknownHostException {
+    }
+
+    @Override
+    public void init() throws Exception{
+        connection.startConnection();
+    }
+
+    @Override
+    public void stop() throws Exception{
+        connection.closeConnection();
+    }
+
+    private Server createServer(){
+        return new Server(5555,data ->{
+            Platform.runLater(()->{
+                messages.appendText(data.toString() + "\n");
+            });
+        });
+    }
+
+    private Client createClient() throws UnknownHostException {
+        return new Client("10.24.87.234", 5555, data ->{
+            Platform.runLater(()->{
+                messages.appendText(data.toString() + "\n");
+            });
+
+        });
+    }
+
     private Parent createContent(){
         messages.setPrefHeight(550);
         TextField input = new TextField();
+        input.setOnAction(event -> {
+            String message = input.getText();
+            input.clear();
+
+            messages.appendText(message+"\n");
+
+            try {
+                connection.send(message);
+            } catch (Exception e) {
+                messages.appendText("Failed to send\n");
+            }
+        });
 
         VBox root = new VBox(20,messages,input);
         return root;
