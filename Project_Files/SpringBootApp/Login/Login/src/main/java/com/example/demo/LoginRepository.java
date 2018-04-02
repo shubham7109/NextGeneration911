efdd66016aef731957cb362e9b9317636cc62b5e
Paul Biermann <paulmb@iastate.edu>
2018-04-02 11:46:38 -0500

    Moved old code from Project_Files/SpringBootApp to Project_Files/ExperimentCode

--- Project_Files/SpringBootApp/Login/Login/src/main/java/com/example/demo/LoginRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LoginRepository extends CrudRepository<Login, String>{
-
-}
