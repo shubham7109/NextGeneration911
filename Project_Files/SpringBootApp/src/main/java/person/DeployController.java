439a99d074459972b72165216648587dff779a6e
Paul <paulmb@iastate.edu>
2018-04-07 17:43:46 -0500

    Added javadocs

--- Project_Files/SpringBootApp/src/main/java/person/DeployController.java
+++ Project_Files/SpringBootApp/src/main/java/person/DeployController.java
@@ -15,29 +15,52 @@ public class DeployController {
 	@Autowired
 	private DeployService deployService; 
 	
+	
+	/**
+	 * @return list of all Deploys
+	 */
 	@RequestMapping("/deploy")
 	public Iterable<Deploy> getAllDeploys()
 	{
 		return deployService.getAllDeploys();
 	}
 	
+	
+	/**
+	 * @param id
+	 * @return deploy with the given id or null if the id doesn't exist
+	 */
 	@RequestMapping("/deploy/{id}")
 	public Deploy getDeploy(@PathVariable("id") String id) {
 		return deployService.getDeploy(id);
 	}
 	
+	
+	/**
+	 * adds a deploy to the database
+	 * @param dep
+	 */
 	@RequestMapping(method=RequestMethod.POST, value="/deploy")
 	public void addDeploy(@RequestBody Deploy dep) 
 	{
 		deployService.addDeploy(dep);
 	}
 	
+	/**
+	 * Updates a deploy in the database
+	 * @param deploy
+	 * @param id
+	 */
 	@RequestMapping(method=RequestMethod.PUT, value="/deploy/{id}")
 	public void updateDeploy(@RequestBody Deploy deploy, @PathVariable("id") String id) 
 	{
 		deployService.updateDeploy(id, deploy);
 	}
 	
+	/**
+	 * Deletes a deploy in the database
+	 * @param id
+	 */
 	@RequestMapping(method=RequestMethod.DELETE, value="/deploy/{id}")
 	public void deleteDeploy(@PathVariable("id") String id)
 	{
