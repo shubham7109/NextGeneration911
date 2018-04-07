aabcec563923e6bc11cced189534b7cf8c66e0ba
Shubham Sharma <shubham@iastate.edu>
2018-04-07 16:59:59 -0500

    Added windows javadoc

--- Project_Files/WindowsApp/Operator911_App/src/operator/Client.java
+++ Project_Files/WindowsApp/Operator911_App/src/operator/Client.java
@@ -4,11 +4,21 @@ import java.io.Serializable;
 import java.net.InetAddress;
 import java.util.function.Consumer;
 
+/**
+ * Class to handle the client's socket calls
+ * @author Shubham Sharma
+ */
 public class Client extends NetworkConnection {
 
     private String ip;
     private int port;
 
+    /**
+     * Constructor to initialize and set up the client
+     * @param ip The IP of the server
+     * @param port The port connecting to the server
+     * @param onRecieveCallback Serializable consumer
+     */
     public Client(String ip, int port, Consumer<Serializable> onRecieveCallback) {
         super(onRecieveCallback);
         this.ip = ip;
