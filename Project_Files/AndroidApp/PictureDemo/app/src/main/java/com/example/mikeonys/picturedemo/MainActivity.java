06985d5c92ac9e71d196673b0a7ed880f1e6c49a
Michael Onyszczak <mikeonys@iastate.edu>
2018-04-14 17:14:11 -0500

    Added permissions for images

--- Project_Files/AndroidApp/PictureDemo/app/src/main/java/com/example/mikeonys/picturedemo/MainActivity.java
+++ Project_Files/AndroidApp/PictureDemo/app/src/main/java/com/example/mikeonys/picturedemo/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.mikeonys.picturedemo;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -20,7 +25,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        requestPermissions(this, requestCode);
+
         imageHolder = (ImageView) findViewById(R.id.captured_photo);
+        imageHolder.setVisibility(View.INVISIBLE);
         Button capturedImageButton = (Button) findViewById(R.id.photo_button);
         capturedImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,5 +46,14 @@ public class MainActivity extends AppCompatActivity {
             Bitmap bitmap = (Bitmap) data.getExtras().get("data");
             imageHolder.setImageBitmap(bitmap);
         }
+    }
+
+    public static void requestPermissions(Activity activity, int requestCode) {
+        ArrayList<String> permissionList = new ArrayList<>();
+        permissionList.add(Manifest.permission.CAMERA);
+        permissionList.add(Manifest.permission.ACCESS_FINE_LOCATION);
+        String[] perms = permissionList.toArray(new String[0]);
+
+        ActivityCompat.requestPermissions(activity, perms, requestCode);
     }
 }
