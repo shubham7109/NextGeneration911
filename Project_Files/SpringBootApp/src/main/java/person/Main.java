1994fa16fc460de59964891a5d4f866869a28386
Paul Biermann <paulmb@iastate.edu>
2018-04-14 18:48:11 -0500

    Added thread to wander Deploys every 5 seconds

--- Project_Files/SpringBootApp/src/main/java/person/Main.java
+++ Project_Files/SpringBootApp/src/main/java/person/Main.java
@@ -8,7 +8,28 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		Thread t1 = new Thread (new Runnable() {
+			
+			@Override
+			public void run() {
+				//sleep for 15 seconds
+				try {
+					Thread.sleep(15000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				
+				while (true) {
+					//DeployWanderer.wander()
+				}
+				
+			}
+		});
+		
+		t1.start();
+		
 		SpringApplication.run(Main.class, args);
+		
 	}
 
 }
