a54d2567d227846e192065d250d8536216b0e6dd
rishabn <rishabn@iastate.edu>
2018-04-14 14:01:04 -0500

    Removed unused variable

--- Project_Files/SpringBootApp/src/main/java/person/AvailableOperatorRouter.java
+++ Project_Files/SpringBootApp/src/main/java/person/AvailableOperatorRouter.java
@@ -12,8 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AvailableOperatorRouter {
-
-	private Client client;
 	
 	@Autowired
 	private LoginService loginService;
