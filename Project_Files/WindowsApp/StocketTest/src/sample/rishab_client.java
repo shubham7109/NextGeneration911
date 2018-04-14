1c6938ab9126654b138b69cea05b730ddaa89c18
Shubham Sharma <shubham@iastate.edu>
2018-04-14 01:13:12 -0500

    Added in new jar file

--- Project_Files/WindowsApp/StocketTest/src/sample/rishab_client.java
+++ Project_Files/WindowsApp/StocketTest/src/sample/rishab_client.java
@@ -14,7 +14,7 @@ public class rishab_client {
         String sentence;
         String modifiedSentence;
         BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-        Socket clientSocket = new Socket("localhost", 6789);
+        Socket clientSocket = new Socket("192.168.56.1", 6789);
         DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
         BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
         sentence = inFromUser.readLine();
