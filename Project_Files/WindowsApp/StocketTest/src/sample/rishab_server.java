a49266430cab45e6871cfa0223af069f131ff851
shubham@iastate.edu <wdshubham@iastate.edu>
2018-04-16 02:12:32 -0500

    Server code now works with multiple clients and is able to send to all connected clients

--- Project_Files/WindowsApp/StocketTest/src/sample/rishab_server.java
+++ Project_Files/WindowsApp/StocketTest/src/sample/rishab_server.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 
 public class rishab_server {
 
-    static final int PORT = 1978;
+    static final int PORT = 1234;
     public static ArrayList<EchoThread> echoThreads = new ArrayList<>();
 
     public static void main(String args[]) {
