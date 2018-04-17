9158bb1d3bd57ca1d27e122ff45f1e053567ea2b
Michael Onyszczak <mikeonys@iastate.edu>
2018-04-17 17:56:09 -0500

    Added encode/decode features for sending bitmaps

---
+++ Project_Files/AndroidApp/PictureDemo/app/src/main/java/com/example/mikeonys/picturedemo/ImageManipulator.java
@@ -0,0 +1,27 @@
+package com.example.mikeonys.picturedemo;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Base64;
+
+import java.io.ByteArrayOutputStream;
+
+/**
+ * Encodes, Decodes, and sends images over internet
+ */
+
+public class ImageManipulator {
+    public static String encodeBase64(Bitmap bitmap){
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 75, byteArrayOutputStream);
+        byte[] b = byteArrayOutputStream.toByteArray();
+
+        return Base64.encodeToString(b, Base64.DEFAULT);
+    }
+
+    public static Bitmap decodeBase64(String s){
+        byte[] decoded = Base64.decode(s, Base64.DEFAULT);
+        return BitmapFactory.decodeByteArray(decoded, 0, decoded.length);
+    }
+
+}
