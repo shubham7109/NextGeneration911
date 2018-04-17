e6ae2f0024916552d46542f4f88814ac2e84a173
Paul Biermann <paulmb@iastate.edu>
2018-04-17 12:53:04 -0500

    removed println() debugging statements that were accidentally left in

--- Project_Files/SpringBootApp/src/main/java/person/DeployWanderer.java
+++ Project_Files/SpringBootApp/src/main/java/person/DeployWanderer.java
@@ -46,8 +46,6 @@ public class DeployWanderer {
 	
 	@EventListener(ApplicationReadyEvent.class)
 	public void initializeCoord() {
-		System.out.println("Initializing Deploy Coordinates");
-		
 		List<Deploy> deploys = deployService.getAllDeploys();
 		
 		//delete each deploy in deploys
@@ -92,8 +90,6 @@ public class DeployWanderer {
 	
 	@Scheduled(fixedRate = 5000)
 	public void wander() {
-		System.out.println("Wandering");
-		
 		List<Deploy> deploy = deployService.getAllDeploys();
 		
 		for (int i = 0; i < deploy.size(); i++) {
