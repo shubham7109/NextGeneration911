8a1599608552327a048c1e13f8322f23e49ffdfa
Mike Onyszczak <mikeonys@iastate.edu>
2018-04-24 09:46:02 -0500

    Started to detect camera rotation

--- Project_Files/AndroidApp/NextGen911_Android_App/app/src/main/java/sb5/cs309/nextgen911/ImageHandler.java
+++ Project_Files/AndroidApp/NextGen911_Android_App/app/src/main/java/sb5/cs309/nextgen911/ImageHandler.java
@@ -2,6 +2,7 @@ package sb5.cs309.nextgen911;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.util.Base64;
 
 import java.io.ByteArrayOutputStream;
@@ -22,6 +23,13 @@ public class ImageHandler {
     public static Bitmap decodeBase64(String s) {
         byte[] decoded = Base64.decode(s, Base64.DEFAULT);
         return BitmapFactory.decodeByteArray(decoded, 0, decoded.length);
+    }
+
+    public static Bitmap rotateImage(Bitmap source, float angle) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(),
+                matrix, true);
     }
 }
 
