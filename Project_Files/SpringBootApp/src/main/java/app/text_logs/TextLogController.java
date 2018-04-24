fb62bca82171a80e2e3706fca234dfc33743fbe5
Paul Biermann <paulmb@iastate.edu>
2018-04-23 21:38:19 -0500

    Implemented TextLogController.java and TextLogService.java

--- Project_Files/SpringBootApp/src/main/java/app/text_logs/TextLogController.java
+++ Project_Files/SpringBootApp/src/main/java/app/text_logs/TextLogController.java
@@ -1,5 +1,26 @@
 package app.text_logs;
 
-public class TextLogController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TextLogController {
+	
+	@Autowired
+	private TextLogService textLogService;
+	
+	@RequestMapping("/textlogs")
+	public List<TextLog> getAllLogs() {
+		return textLogService.getAllLogs();
+	}
+	
+	@RequestMapping(method=RequestMethod.POST, value="/textlogs")
+	public void addTextLog(@RequestBody TextLog logs) {
+		textLogService.addLog(logs);
+	}
 }
