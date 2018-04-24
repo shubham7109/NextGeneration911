a6c82f6e6832897580bff9002fc778674ff6cc8c
Paul Biermann <paulmb@iastate.edu>
2018-04-23 22:43:36 -0500

    removed testlogs

--- Project_Files/SpringBootApp/src/main/java/app/text_logs/TextLogRepository.java
+++
@@ -1,9 +0,0 @@
-package app.text_logs;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TextLogRepository extends CrudRepository<TextLog, String> {
-
-}
